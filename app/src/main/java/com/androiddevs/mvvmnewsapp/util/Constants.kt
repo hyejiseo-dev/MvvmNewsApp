82b6b5904baacf9d998d73295e79b5dce511fd57
hyejiseo-dev <dianna537@gmail.com>
2022-03-15 23:11:25 +0900

    Mvvm 초기 세팅, retrofit 구조 적용, Article 테이블 DAO 생성

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY= "baa50c41dfc543fb924abe55e2d36141"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
