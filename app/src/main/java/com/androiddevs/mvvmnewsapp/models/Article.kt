18bcc9b9c300ecd7568e060dbfe78d83b762214a
hyejiseo-dev <dianna537@gmail.com>
2022-03-18 14:42:09 +0900

    페이징 기능 추가, 스크롤 시 프로그래스 바 보이기

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -14,8 +14,8 @@ data class Article(
     val content: String?,
     val description: String?,
     val publishedAt: String?,
-    val source: Source,   //Source 클래스를 DB가 읽을 수 있도록 String 형식으로 바꿔주는 Converter가 필요!
+    val source: Source?,   //Source 클래스를 DB가 읽을 수 있도록 String 형식으로 바꿔주는 Converter가 필요!
     val title: String?,
-    val url: String,
-    val urlToImage: String
+    val url: String?,
+    val urlToImage: String?
 ) : Serializable
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -17,8 +17,11 @@ class NewsViewModel(
     val breakingNews:MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
 
+    var breakingNewsResponse: NewsResponse? = null
+
     val searchNews:MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var searchNewsPage = 1
+    var searchNewsResponse: NewsResponse? = null
 
     init {
         getBreakingNews("kr")
@@ -35,7 +38,16 @@ class NewsViewModel(
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse>{
         if(response.isSuccessful){
             response.body()?.let{
-                resultResponse -> return Resource.Success(resultResponse)
+                resultResponse ->
+                breakingNewsPage++
+                if(breakingNewsResponse == null){
+                    breakingNewsResponse = resultResponse
+                }else{
+                    val oldArticles = breakingNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(breakingNewsResponse?: resultResponse)
             }
         }
         return Resource.Error(response.message())
@@ -52,7 +64,16 @@ class NewsViewModel(
     private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse>{
         if(response.isSuccessful){
             response.body()?.let{
-                    resultResponse -> return Resource.Success(resultResponse)
+                    resultResponse ->
+                searchNewsPage++
+                if(searchNewsResponse == null){
+                    searchNewsResponse = resultResponse
+                }else{
+                    val oldArticles = searchNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(searchNewsResponse?: resultResponse)
             }
         }
         return Resource.Error(response.message())
